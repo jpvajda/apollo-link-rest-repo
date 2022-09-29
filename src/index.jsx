b6a0a7bf7470227f7eb5c00b3900d571fd1a6f5a
John P. Vajda <jvajda@apollographql.com>
2022-09-29 17:39:14 -0600

    makes changes foer apollo-link-rest

--- src/index.jsx
+++ src/index.jsx
@@ -1,101 +1,27 @@
 /*** APP ***/
-import React, { useState } from "react";
+import { RestLink } from 'apollo-link-rest'
 import { createRoot } from "react-dom/client";
 import {
   ApolloClient,
   ApolloProvider,
   InMemoryCache,
-  gql,
-  useQuery,
-  useMutation,
 } from "@apollo/client";
 
-import { link } from "./link.js";
-import "./index.css";
+// Set `RestLink` with your endpoint
+const restLink = new RestLink({ uri: "https://swapi.dev/api/" });
 
-const ALL_PEOPLE = gql`
-  query AllPeople {
-    people {
-      id
-      name
-    }
-  }
-`;
-
-const ADD_PERSON = gql`
-  mutation AddPerson($name: String) {
-    addPerson(name: $name) {
-      id
-      name
-    }
-  }
-`;
-
-function App() {
-  const [name, setName] = useState('');
-  const {
-    loading,
-    data,
-  } = useQuery(ALL_PEOPLE);
-
-  const [addPerson] = useMutation(ADD_PERSON, {
-    update: (cache, { data: { addPerson: addPersonData } }) => {
-      const peopleResult = cache.readQuery({ query: ALL_PEOPLE });
-
-      cache.writeQuery({
-        query: ALL_PEOPLE,
-        data: {
-          ...peopleResult,
-          people: [
-            ...peopleResult.people,
-            addPersonData,
-          ],
-        },
-      });
-    },
-  });
-
-  return (
-    <main>
-      <h1>Apollo Client Issue Reproduction</h1>
-      <p>
-        This application can be used to demonstrate an error in Apollo Client.
-      </p>
-      <div className="add-person">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={evt => setName(evt.target.value)}
-        />
-        <button
-          onClick={() => {
-            addPerson({ variables: { name } });
-            setName('');
-          }}
-        >
-          Add person
-        </button>
-      </div>
-      <h2>Names</h2>
-      {loading ? (
-        <p>Loading…</p>
-      ) : (
-        <ul>
-          {data?.people.map(person => (
-            <li key={person.id}>{person.name}</li>
-          ))}
-        </ul>
-      )}
-    </main>
-  );
-}
-
+// Setup your client
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link
+  link: restLink
+
 });
+
+// Invoke the query and log the person's name
+client.query({ query }).then(response => {
+  console.log(response.data.person.name);
+});
+
 
 const container = document.getElementById("root");
 const root = createRoot(container);
